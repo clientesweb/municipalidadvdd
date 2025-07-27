b952130a4766c6e82f4316962bfd5d23e4f915b4
clientesweb <alexisifran32@gmail.com>
2025-07-27 15:38:16 -0300

    Actualizar page.tsx

--- app/turistas/donde-alojarme/page.tsx
+++ app/turistas/donde-alojarme/page.tsx
@@ -21,7 +21,7 @@ export default function DondeAlojarme() {
   const rightMenuItems = [
     { name: "Circuitos", href: "/turistas/circuitos" },
     { name: "Servicios", href: "/vecinos/servicios" },
-    { name: "Noticias", href: "/vecinos/noticias" },
+    { name: "Noticias", href: "/noticias" },
   ]
 
   const allMenuItems = [...leftMenuItems, ...rightMenuItems]
