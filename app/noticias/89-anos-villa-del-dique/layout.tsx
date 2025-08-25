39477205401d309d9c3a6cebad146bd8b0cef522
clientesweb <alexisifran32@gmail.com>
2025-08-25 17:49:46 -0300

    Actualizar layout.tsx

--- app/noticias/89-anos-villa-del-dique/layout.tsx
+++ app/noticias/89-anos-villa-del-dique/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
     title: "Villa del Dique celebró sus 89 años con obras, homenajes y una gran participación popular",
     description:
       "En el marco del 89° aniversario de Villa del Dique, el intendente Emiliano Torres encabezó los actos conmemorativos y anunció una inversión de 150 millones de pesos en nuevas maquinarias.",
-    images: ["/images/89-años-villa-del-dique-1.jpg"],
+    images: ["/images/89-anos-villa-del-dique-1.jpg"],
   },
 }
 
